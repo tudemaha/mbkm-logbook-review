f3e0f8e085b1f80e98e590312280dcb5dc3047e7
tudemaha <gede_mahardika88@yahoo.co.id>
2024-10-11 15:19:01 +0800

    feat: get logbooks by mentee id

--- src/app/api/mentees/[menteeId]/route.js
+++ src/app/api/mentees/[menteeId]/route.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
+export async function GET(req, { params }) {
   const authorization = req.headers?.get('authorization');
   const url = new URL(req.url);
-  const programId = url.searchParams.get('programId');
+  const activityId = url.searchParams.get('activityId');
+  const { menteeId } = params ;
 
   const apiHost = process.env.API_HOST;
-  const apiPath = `/v1alpha1/mentors/me/mentees?program_id=${programId}&offset=0&limit=50`;
+  const apiPath = `/v1alpha2/mentors/activity/${activityId}/mentees/${menteeId}`;
 
   try {
     const res = await axios.get(apiHost + apiPath, {
@@ -16,8 +17,8 @@ export async function GET(req) {
         "Content-Type": 'application/json',
       }
     })
-    const { status, data, headers } = res;
-    return NextResponse.json(data, { status, headers });
+    const { status, data } = res;
+    return NextResponse.json(data, { status });
   } catch (error) {
     const { status, data } = error.response;
     return NextResponse.json(data, { status });
--- src/app/logbooks/[menteeId]/page.jsx
+++ src/app/logbooks/[menteeId]/page.jsx
@@ -1,9 +1,76 @@
 "use client";
 
+import { getJsonItem } from "@/app/utils/localStorage";
 import Header from "@/components/Header";
+import axios from "axios";
+import { notFound, useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { Row, Col, Table, Button, Badge, Container } from "react-bootstrap";
 
 export default function LogbookList() {
+	const router = useRouter();
+	const { menteeId } = useParams();
+
+	const [mentee, setMentee] = useState({});
+	const [logbooks, setLogbooks] = useState([]);
+
+	useEffect(() => {
+		const user = getJsonItem("logrev-user");
+		if (!user) {
+			router.push("/login");
+			return;
+		}
+
+		const fetchMentee = async () => {
+			try {
+				const res = await axios.get(
+					`/api/mentees/${menteeId}?activityId=${user.activity_id}`,
+					{
+						headers: {
+							Authorization: user.access,
+						},
+					}
+				);
+				setMentee(res.data.data);
+			} catch (error) {
+				if (error.response.status === 400) {
+					router.push("/404");
+				}
+				if (error.response.status === 401) {
+					router.push("/login");
+					return;
+				}
+			}
+		};
+		fetchMentee();
+	}, [menteeId, router]);
+
+	useEffect(() => {
+		const user = getJsonItem("logrev-user");
+
+		const fetchLogbooks = async () => {
+			try {
+				if (mentee.id_reg_penawaran) {
+					const res = await axios.get(
+						`/api/logbooks?penawaranId=${mentee.id_reg_penawaran}`,
+						{
+							headers: {
+								Authorization: user.access,
+							},
+						}
+					);
+					setLogbooks(res.data.data);
+				}
+			} catch (error) {
+				if (error.response.status === 401) {
+					router.push("/login");
+					return;
+				}
+			}
+		};
+		fetchLogbooks();
+	}, [router, mentee, menteeId]);
+
 	return (
 		<>
 			<Header />
@@ -13,7 +80,7 @@ export default function LogbookList() {
 				</Button>
 				<Row className="justify-content-center">
 					<h3 className="text-center">Logbook of</h3>
-					<h4 className="text-center mb-4">Dummy Name</h4>
+					<h4 className="text-center mb-4">{mentee.name}</h4>
 					<Col md="8">
 						<Table striped hover>
 							<thead>
@@ -24,40 +91,39 @@ export default function LogbookList() {
 								</tr>
 							</thead>
 							<tbody>
-								<tr>
-									<td>Dummy Name</td>
-									<td>
-										<Badge bg="danger" pill>
-											0
-										</Badge>
-									</td>
-									<td>
-										<Button
-											variant="dark"
-											size="sm"
-											className="badge rounded-pill"
-										>
-											Details
-										</Button>
-									</td>
-								</tr>
-								<tr>
-									<td>Dummy Name</td>
-									<td>
-										<Badge bg="danger" pill>
-											0
-										</Badge>
-									</td>
-									<td>
-										<Button
-											variant="dark"
-											size="sm"
-											className="badge rounded-pill"
-										>
-											Details
-										</Button>
-									</td>
-								</tr>
+								{logbooks.map((logbook) => {
+									return (
+										<tr key={logbook.id}>
+											<td>{`${logbook.start_date} - ${logbook.end_date}`}</td>
+											<td>
+												<Badge
+													bg={
+														logbook.status === "DRAFT"
+															? "primary"
+															: logbook.status === "SUBMITTED"
+															? "warning"
+															: logbook.status === "ACCEPTED"
+															? "success"
+															: "danger"
+													}
+													pill
+												>
+													{logbook.status}
+												</Badge>
+											</td>
+											<td>
+												<Button
+													variant="dark"
+													size="sm"
+													className="badge rounded-pill"
+													href={`/logbooks/${menteeId}/${logbook.id}`}
+												>
+													Details
+												</Button>
+											</td>
+										</tr>
+									);
+								})}
 							</tbody>
 						</Table>
 					</Col>
